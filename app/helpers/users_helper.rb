80c201d5bb5708b6ae2cfbc1a7173f51fe202ed5
Yuko <naka.u926@gmail.com>
2019-04-25 08:40:03 +0000

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def gravatar_for(user, size: 80)
+    # オプション引数よりもキーワード引数の方が簡潔に実装できる
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
-class User < ApplicationRecordU
+class User < ApplicationRecord
   before_save { self.email = email.downcase }
   # selfは現在のユーザーを指す 右は省略
 
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255}
-                    format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: {maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
